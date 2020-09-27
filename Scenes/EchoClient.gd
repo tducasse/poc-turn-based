13be34a9f957a468af2a03189e39fc38a7e8bf3c
Thibaud Ducasse <thibaud.ducasse@gmail.com>
2020-09-27 23:34:48 +1000

    Add websocket client

---
+++ Scenes/EchoClient.gd
@@ -0,0 +1,76 @@
+extends Control
+
+# The URL we will connect to
+var websocket_url = "ws://localhost:3000" if OS.is_debug_build() else "ws://projectv3.herokuapp.com"
+
+# Our WebSocketClient instance
+var _client = WebSocketClient.new()
+
+onready var chat = $Chat
+
+func _ready():
+	# Connect base signals to get notified of connection open, close, and errors.
+	_client.connect("connection_closed", self, "_closed")
+	_client.connect("connection_error", self, "_closed")
+	_client.connect("connection_established", self, "_connected")
+	# This signal is emitted when not using the Multiplayer API every time
+	# a full packet is received.
+	# Alternatively, you could check get_peer(1).get_available_packets() in a loop.
+	_client.connect("data_received", self, "_on_data")
+
+	# Initiate connection to the given URL.
+	var err = _client.connect_to_url(websocket_url)
+	if err != OK:
+		print("Unable to connect")
+		set_process(false)
+
+func _closed(was_clean = false):
+	# was_clean will tell you if the disconnection was correctly notified
+	# by the remote peer before closing the socket.
+	print("Closed, clean: ", was_clean)
+	set_process(false)
+
+func _connected(proto = ""):
+	# This is called on connection, "proto" will be the selected WebSocket
+	# sub-protocol (which is optional)
+	print("Connected with protocol: ", proto)
+	# You MUST always use get_peer(1).put_packet to send data to server,
+	# and not put_packet directly when not using the MultiplayerAPI.
+	_client.get_peer(1).put_packet(JSON.print(["new-connection", _client.get_unique_id()]).to_utf8())
+
+func _on_data():
+	# Print the received packet, you MUST always use get_peer(1).get_packet
+	# to receive data from server, and not get_packet directly when not
+	# using the MultiplayerAPI.
+	var data = _client.get_peer(1).get_packet().get_string_from_utf8()
+
+	if not data:
+		return
+	
+	data = parse_json(data)
+	if not typeof(data) == TYPE_ARRAY:
+		return
+
+	var type = data[0]
+	var payload = data[1]
+	
+	match type:
+		"new-message":
+			add_new_messages(payload)
+		_:
+			print(type + ": not supported")
+	
+
+func _process(_delta):
+	# Call this in _process or _physics_process. Data transfer, and signals
+	# emission will only happen when calling this function.
+	_client.poll()
+	
+func add_new_message(message):
+	print(message)
+	chat.text +=message + '\n'
+	
+func add_new_messages(messages):
+	print(messages)
+	for message in messages:
+		add_new_message(message)
--- server/index.js
+++ server/index.js
@@ -6,23 +6,47 @@ const ws = new WebSocket.Server({ port: PORT });
 
 const messages = ["Start chatting"];
 
+const sendNewMessage = (message) => {
+  messages.push(message);
+  ws.clients.forEach((client) => {
+    if (client.readyState === WebSocket.OPEN) {
+      client.send(JSON.stringify(["new-message", [message]]));
+    }
+  });
+};
+
+const sendNewMessages = (m) => m.forEach(sendNewMessage);
+
 ws.on("connection", (socketClient) => {
   console.log("connected");
   console.log("client Set length: ", ws.clients.size);
 
-  socketClient.send(JSON.stringify(messages));
+  // send all previous messages to every client
+  socketClient.send(JSON.stringify(["new-message", messages]));
 
   socketClient.on("close", () => {
     console.log("closed");
     console.log("Number of clients: ", ws.clients.size);
   });
 
-  socketClient.on("message", (message) => {
-    messages.push(message);
-    ws.clients.forEach((client) => {
-      if (client.readyState === WebSocket.OPEN) {
-        client.send(JSON.stringify([message]));
-      }
-    });
+  socketClient.on("message", (rawData) => {
+    let data;
+    try {
+      data = JSON.parse(rawData);
+    } catch (err) {
+      console.error(`Could not parse ${rawData}`);
+    }
+
+    const [type, payload] = data;
+    switch (type) {
+      case "new-message":
+        sendNewMessages(payload);
+        break;
+      case "new-connection":
+        sendNewMessage(`New peer connected: ${payload}`);
+        break;
+      default:
+        console.log(`${type}: not supported`);
+    }
   });
 });
