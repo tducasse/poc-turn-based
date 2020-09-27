c33ed3616e9b95e6c376d9697d9d30dffa5a90d1
unknown <charlanlamy@gmail.com>
2020-09-27 15:33:12 +0200

    Add timer to get ressource from income

--- UI/UI.gd
+++ UI/UI.gd
@@ -6,11 +6,16 @@ var income = 0 setget set_income
 
 onready var ressourceLabel := $MarginContainer/TopLine/RessourceLabel
 onready var incomeLabel := $MarginContainer/TopLine/IncomeLabel
+onready var prepTimerLabel := $MarginContainer/TopLine/PrepTimerLabel
+onready var prepTimer := $MarginContainer/TopLine/PrepTimerLabel/PrepTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	ressourceLabel.text = "Ressource : %d" % ressource
-	incomeLabel.text = "Income : %d" % income
+	update_labels()
+
+
+func _process(_delta):
+	update_prep_label()
 
 
 func on_ressource_spent(cost: int, incomeValue: int):
@@ -27,9 +32,25 @@ func earn_income():
 
 func set_ressource(value: int):
 	ressource = value
-	ressourceLabel.text = "Ressource : %d" % ressource
+	update_labels()
 
 
 func set_income(value: int):
 	income = value
-	incomeLabel.text = "Income : %d" % income
+	update_labels()
+
+
+
+func update_labels():
+	ressourceLabel.bbcode_text = "[center]Ressource : %d[/center]" % ressource
+	incomeLabel.bbcode_text = "[center]Income : %d[/center]" % income
+	update_prep_label()
+
+
+func update_prep_label():
+	prepTimerLabel.bbcode_text = "[center]%d[/center]" % prepTimer.time_left
+
+
+func _on_PrepTimer_timeout():
+	earn_income()
+	prepTimer.start()
