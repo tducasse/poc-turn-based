2fd225a34f89aab90aa55902c4cf036a2ee83832
Thibaud Ducasse <thibaud.ducasse@gmail.com>
2020-10-10 19:04:44 +1100

    Replace font + fix styles

--- Scenes/Game/ReadyPopup.gd
+++ Scenes/Game/ReadyPopup.gd
@@ -3,7 +3,7 @@ extends AcceptDialog
 func _ready():
 	var label = get_label()
 	var ok = get_ok()
-	var font = load("res://Fonts/Assurant-Standard.tres")
+	var font = load("res://Fonts/PixelSansSerif.tres")
 	label.add_font_override('font', font)
 	ok.add_font_override('font', font)
 
